16b2d142e5e1fb3359c4e51fee19e68a67f412af
baka4n <qq474899581@gmail.com>
2023-04-11 16:03:35 +0800

    依赖于api

--- nucleoplasm-command-level/src/main/java/union/xenfork/nucleoplasm/command/level/NucleoplasmCommandLevel.java
+++ nucleoplasm-command-level/src/main/java/union/xenfork/nucleoplasm/command/level/NucleoplasmCommandLevel.java
@@ -3,8 +3,8 @@ package union.xenfork.nucleoplasm.command.level;
 import net.fabricmc.api.ModInitializer;
 
 public class NucleoplasmCommandLevel implements ModInitializer {
+    public static final String modid = "nucleoplasm_command_level";
     @Override
     public void onInitialize() {
-
     }
 }
