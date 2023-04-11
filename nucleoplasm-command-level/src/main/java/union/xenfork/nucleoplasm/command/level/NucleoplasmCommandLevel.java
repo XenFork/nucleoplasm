988134204da2b29534ffc20f8328670c0c216a7b
baka4n <qq474899581@gmail.com>
2023-04-11 15:46:02 +0800

    添加权限组管理mod

---
+++ nucleoplasm-command-level/src/main/java/union/xenfork/nucleoplasm/command/level/NucleoplasmCommandLevel.java
@@ -0,0 +1,10 @@
+package union.xenfork.nucleoplasm.command.level;
+
+import net.fabricmc.api.ModInitializer;
+
+public class NucleoplasmCommandLevel implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ nucleoplasm-command-level/src/main/java/union/xenfork/nucleoplasm/command/level/server/NucleoplasmCommandLevelServer.java
@@ -0,0 +1,10 @@
+package union.xenfork.nucleoplasm.command.level.server;
+
+import net.fabricmc.api.DedicatedServerModInitializer;
+
+public class NucleoplasmCommandLevelServer implements DedicatedServerModInitializer {
+    @Override
+    public void onInitializeServer() {
+
+    }
+}
