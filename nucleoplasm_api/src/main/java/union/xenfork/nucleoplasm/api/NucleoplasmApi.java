8478160136b763bed39e77ecee0d889b64033476
baka4n <qq474899581@gmail.com>
2023-04-11 14:15:56 +0800

    添置数据库

--- nucleoplasm_api/src/main/java/union/xenfork/nucleoplasm/api/NucleoplasmApi.java
+++ nucleoplasm_api/src/main/java/union/xenfork/nucleoplasm/api/NucleoplasmApi.java
@@ -1,10 +1,20 @@
 package union.xenfork.nucleoplasm.api;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.util.Identifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class NucleoplasmApi implements ModInitializer {
+    public static final String modid = "nucleoplasm_api";
+    public static final Logger logger = LoggerFactory.getLogger(modid);
     @Override
     public void onInitialize() {
 
     }
+
+    public static Identifier getIdentifier(String name) {
+        return new Identifier(modid, name);
+    }
+
 }
--- nucleoplasm_api/src/main/java/union/xenfork/nucleoplasm/api/server/NucleoplasmApiServer.java
+++ nucleoplasm_api/src/main/java/union/xenfork/nucleoplasm/api/server/NucleoplasmApiServer.java
@@ -5,6 +5,5 @@ import net.fabricmc.api.DedicatedServerModInitializer;
 public class NucleoplasmApiServer implements DedicatedServerModInitializer {
     @Override
     public void onInitializeServer() {
-
     }
 }
