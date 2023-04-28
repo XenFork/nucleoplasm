af164097202c03a90854ad74bd336cd1b91fbbb3
baka4n <qq474899581@gmail.com>
2023-04-28 23:09:37 +0800

    update

--- nucleoplasm/chemistry/src/main/java/union/xenfork/nucleoplasm/chemistry/quickio/ItemEntity.java
+++ nucleoplasm/chemistry/src/main/java/union/xenfork/nucleoplasm/chemistry/quickio/ItemEntity.java
@@ -2,6 +2,7 @@ package union.xenfork.nucleoplasm.chemistry.quickio;
 
 import com.github.artbits.quickio.core.IOEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.collection.DefaultedList;
 import union.xenfork.nucleoplasm.api.quickio.minecraft.IItemStack;
 
 import java.util.List;
@@ -9,8 +10,33 @@ import java.util.UUID;
 
 public class ItemEntity extends IOEntity {
     public String name;
-    public List<IItemStack> stacks;
+    public DefaultedList<IItemStack> stacks;
     public ItemEntity(String name) {
-        String ch = "";
+        var ref = new Object() {
+            String ch = "";
+            int count = 0;
+            int b = 0;
+        };
+
+
+        name.chars().forEach(value -> {
+            switch (ref.b) {
+                case 0 -> {
+                    
+                }
+            }
+            char c = name.charAt(value);
+            if (c >= 'A' && c <= 'Z') {
+                ref.b = 0;
+                ref.ch += String.valueOf(c);
+
+            } else if (c >= 'a'&& c <= 'z') {
+                ref.b = 1;
+                ref.ch += String.valueOf(c);
+            } else if (c >= '1'&& c <= '9') {
+                ref.b = 2;
+                ref.count += ref.count * 10 + Integer.parseInt(String.valueOf(c));
+            }
+        });
     }
 }
