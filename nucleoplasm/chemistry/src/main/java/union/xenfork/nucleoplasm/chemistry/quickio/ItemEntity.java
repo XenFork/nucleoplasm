eaf153d274abff28760c1fedeee2b4c2da9ed91b
baka4n <qq474899581@gmail.com>
2023-05-06 20:49:06 +0800

    Update ItemEntity.java

--- nucleoplasm/chemistry/src/main/java/union/xenfork/nucleoplasm/chemistry/quickio/ItemEntity.java
+++ nucleoplasm/chemistry/src/main/java/union/xenfork/nucleoplasm/chemistry/quickio/ItemEntity.java
@@ -1,42 +1,48 @@
 package union.xenfork.nucleoplasm.chemistry.quickio;
 
 import com.github.artbits.quickio.core.IOEntity;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.registry.Registries;
+import net.minecraft.util.Identifier;
 import net.minecraft.util.collection.DefaultedList;
+import union.xenfork.nucleoplasm.api.NucleoplasmApi;
 import union.xenfork.nucleoplasm.api.quickio.minecraft.IItemStack;
+import union.xenfork.nucleoplasm.chemistry.Nucleoplasm;
 
 import java.util.List;
+import java.util.Locale;
 import java.util.UUID;
 
 public class ItemEntity extends IOEntity {
     public String name;
     public DefaultedList<IItemStack> stacks;
     public ItemEntity(String name) {
+        stacks = DefaultedList.of();
         var ref = new Object() {
             String ch = "";
             int count = 0;
-            int b = 0;
+            boolean b = false;
         };
-
-
         name.chars().forEach(value -> {
-            switch (ref.b) {
-                case 0 -> {
-                    
-                }
-            }
             char c = name.charAt(value);
             if (c >= 'A' && c <= 'Z') {
-                ref.b = 0;
-                ref.ch += String.valueOf(c);
-
+                if (!ref.b) {
+                    ref.b = true;
+                    ref.ch += String.valueOf(c);
+                } else {
+                    String lowerCase = ref.ch.toLowerCase(Locale.ROOT);
+                    Item item = Registries.ITEM.get(Identifier.of(Nucleoplasm.modid, lowerCase));
+                    ItemStack stack = new ItemStack(item, ref.count > 0 ? ref.count : 1);
+                    stacks.add(new IItemStack(stack));
+                }
             } else if (c >= 'a'&& c <= 'z') {
-                ref.b = 1;
                 ref.ch += String.valueOf(c);
-            } else if (c >= '1'&& c <= '9') {
-                ref.b = 2;
+            } else if (c >= '0'&& c <= '9') {
                 ref.count += ref.count * 10 + Integer.parseInt(String.valueOf(c));
             }
         });
+        ItemStack stack = new ItemStack(Registries.ITEM.get(Identifier.of(Nucleoplasm.modid, ref.ch.toLowerCase(Locale.ROOT))))
     }
+
 }
