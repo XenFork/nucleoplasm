285951466dfcfcc5f5ddc1258fcede3698dff568
baka4n <qq474899581@gmail.com>
2023-05-18 16:18:53 +0800

    update

--- nucleoplasm/json-edit/src/main/java/union/xenfork/nucleoplasm/json/edit/mixin/MixinLootManage.java
+++ nucleoplasm/json-edit/src/main/java/union/xenfork/nucleoplasm/json/edit/mixin/MixinLootManage.java
@@ -1,26 +1,33 @@
 package union.xenfork.nucleoplasm.json.edit.mixin;
 
 import net.fabricmc.fabric.api.loot.v2.LootTableEvents;
+import net.minecraft.loot.LootDataType;
 import net.minecraft.loot.LootManager;
 import net.minecraft.resource.ResourceManager;
 import net.minecraft.resource.ResourceReloader;
+import net.minecraft.util.Identifier;
 import net.minecraft.util.profiler.Profiler;
 import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 import java.nio.file.OpenOption;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executor;
 
 @Mixin(LootManager.class)
-public class MixinLootManage {
+public abstract class MixinLootManage {
 
-    @Inject(method = "reload", at = @At(value = "INVOKE", target = "Ljava/util/Objects;requireNonNull(Ljava/lang/Object;)Ljava/lang/Object;", shift = At.Shift.AFTER))
-    public void reload(ResourceReloader.Synchronizer synchronizer, ResourceManager manager, Profiler prepareProfiler, Profiler applyProfiler, Executor prepareExecutor, Executor applyExecutor, CallbackInfoReturnable<CompletableFuture<Void>> cir) {
-
-    }
+//    @Inject(method = "reload", at = @At(value = "INVOKE", target = "Ljava/util/Objects;requireNonNull(Ljava/lang/Object;)Ljava/lang/Object;", shift = At.Shift.AFTER))
+//    public void reload(ResourceReloader.Synchronizer synchronizer, ResourceManager manager, Profiler prepareProfiler, Profiler applyProfiler, Executor prepareExecutor, Executor applyExecutor, CallbackInfoReturnable<CompletableFuture<Void>> cir) {
+//
+//    }
 }
