91e2ccf56e0087612a85196874ebeaf2811f41d6
baka4n <qq474899581@gmail.com>
2023-05-20 15:04:25 +0800

    upload

--- nucleoplasm/json-edit/src/main/java/union/xenfork/nucleoplasm/json/edit/mixin/MixinRecipeManage.java
+++ nucleoplasm/json-edit/src/main/java/union/xenfork/nucleoplasm/json/edit/mixin/MixinRecipeManage.java
@@ -1,30 +1,30 @@
-//package union.xenfork.nucleoplasm.json.edit.mixin;
-//
-//import com.google.gson.JsonElement;
-//import net.minecraft.recipe.RecipeManager;
-//import net.minecraft.resource.ResourceManager;
-//import net.minecraft.util.Identifier;
-//import net.minecraft.util.profiler.Profiler;
-//import org.spongepowered.asm.mixin.Mixin;
-//import org.spongepowered.asm.mixin.injection.At;
-//import org.spongepowered.asm.mixin.injection.Inject;
-//import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-//
-//import java.io.File;
-//import java.util.HashMap;
-//import java.util.Map;
-//
-//import static union.xenfork.nucleoplasm.json.edit.Nucleoplasm.*;
-//
-//@Mixin(RecipeManager.class)
-//public class MixinRecipeManage {
-//    @Inject(method = "apply(Ljava/util/Map;Lnet/minecraft/resource/ResourceManager;Lnet/minecraft/util/profiler/Profiler;)V", at = @At("HEAD"))
-//    private void apply(Map<Identifier, JsonElement> map, ResourceManager resourceManager, Profiler profiler, CallbackInfo ci) {
-//        outputJson(map, recipe);
-//
+package union.xenfork.nucleoplasm.json.edit.mixin;
+
+import com.google.gson.JsonElement;
+import net.minecraft.recipe.RecipeManager;
+import net.minecraft.resource.ResourceManager;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.profiler.Profiler;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
+import static union.xenfork.nucleoplasm.json.edit.Nucleoplasm.*;
+
+@Mixin(RecipeManager.class)
+public class MixinRecipeManage {
+    @Inject(method = "apply(Ljava/util/Map;Lnet/minecraft/resource/ResourceManager;Lnet/minecraft/util/profiler/Profiler;)V", at = @At("HEAD"))
+    private void apply(Map<Identifier, JsonElement> map, ResourceManager resourceManager, Profiler profiler, CallbackInfo ci) {
+        outputJson(map, recipe);
+
 //        map.clear();
 //        Map<Identifier, File> iFile = new HashMap<>();
 //        load(recipe.toFile(), new StringBuilder(), iFile);
 //        parser(map, iFile);
-//    }
-//}
+    }
+}
