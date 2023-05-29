afa77f5145cd8ae0bdc2f7440f62e1e0a10682cc
baka4n <qq474899581@gmail.com>
2023-05-30 01:34:21 +0800

    测试新方案，模仿fabric的加载机制

--- nucleoplasm/json-edit/src/main/java/union/xenfork/nucleoplasm/json/edit/mixin/MixinDataJsonLoader.java
+++ nucleoplasm/json-edit/src/main/java/union/xenfork/nucleoplasm/json/edit/mixin/MixinDataJsonLoader.java
@@ -2,9 +2,12 @@ package union.xenfork.nucleoplasm.json.edit.mixin;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonElement;
+import net.fabricmc.fabric.mixin.resource.conditions.SinglePreparationResourceReloaderMixin;
+import net.minecraft.loot.LootManager;
 import net.minecraft.resource.*;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.JsonHelper;
+import net.minecraft.util.profiler.Profiler;
 import org.slf4j.Logger;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -26,7 +29,7 @@ import java.util.Map;
 import java.util.stream.Stream;
 
 @Mixin(JsonDataLoader.class)
-public class MixinDataJsonLoader {
+public class MixinDataJsonLoader extends MixinSinglePreparationResourceReloader {
     @Shadow
     @Final
     private static Logger LOGGER;
@@ -36,7 +39,20 @@ public class MixinDataJsonLoader {
     @Shadow
     @Final
     private Gson gson;
+
+    @SuppressWarnings("unchecked")
+    @Override
+    protected void nucleoplasm_synchronizer_lambda(ResourceManager resourceManager, Profiler profiler, Object prepared) {
+        profiler.push("Fabric resource conditions: %s".formatted(dataType));
+        Map<Identifier, JsonElement> prepared1 = (Map<Identifier, JsonElement>) prepared;
+        prepared1.forEach((id, json) -> {
+            System.out.println(id);
+        });
+    }
+
     private static final Map<String, Boolean> hasCreate = new HashMap<>();
+
+
 
     private static void validate(String dataType) {
         try(Stream<Path> list = Files.list(Nucleoplasm.dir)) {
