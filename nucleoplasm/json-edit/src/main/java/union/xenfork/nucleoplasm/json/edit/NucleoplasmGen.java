7945401162f7ab13f06cf7255a6e964c74c286a5
baka4n <qq474899581@gmail.com>
2023-06-02 16:57:19 +0800

    先上传

---
+++ nucleoplasm/json-edit/src/main/java/union/xenfork/nucleoplasm/json/edit/NucleoplasmGen.java
@@ -0,0 +1,11 @@
+package union.xenfork.nucleoplasm.json.edit;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class NucleoplasmGen implements DataGeneratorEntrypoint {
+    @Override
+    public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+        fabricDataGenerator.fabricOutput
+    }
+}
---
+++ nucleoplasm/json-edit/src/main/java/union/xenfork/nucleoplasm/json/edit/mixin/MixinDataProvider.java
@@ -0,0 +1,8 @@
+package union.xenfork.nucleoplasm.json.edit.mixin;
+
+import net.minecraft.data.DataProvider;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(DataProvider.class)
+public class MixinDataProvider {
+}
