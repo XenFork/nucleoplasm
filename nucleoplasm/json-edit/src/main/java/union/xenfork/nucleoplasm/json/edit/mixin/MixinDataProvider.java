12138b2eaa2f6510d7fcf39768aa969fcc1d3f8a
baka4n <qq474899581@gmail.com>
2023-06-02 16:57:49 +0800

    先上传2

--- nucleoplasm/json-edit/src/main/java/union/xenfork/nucleoplasm/json/edit/mixin/MixinDataProvider.java
+++
@@ -1,8 +0,0 @@
-package union.xenfork.nucleoplasm.json.edit.mixin;
-
-import net.minecraft.data.DataProvider;
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin(DataProvider.class)
-public class MixinDataProvider {
-}
