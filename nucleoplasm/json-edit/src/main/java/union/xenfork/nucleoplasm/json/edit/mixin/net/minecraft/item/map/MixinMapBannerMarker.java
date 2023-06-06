c8c2dd5e9ae487624d1540bbebce4c865e8e4a2a
baka4n <qq474899581@gmail.com>
2023-06-06 09:38:43 +0800

    update

--- nucleoplasm/json-edit/src/main/java/union/xenfork/nucleoplasm/json/edit/mixin/net/minecraft/item/map/MixinMapBannerMarker.java
+++ nucleoplasm/json-edit/src/main/java/union/xenfork/nucleoplasm/json/edit/mixin/net/minecraft/item/map/MixinMapBannerMarker.java
@@ -1,10 +1,26 @@
 package union.xenfork.nucleoplasm.json.edit.mixin.net.minecraft.item.map;
 
 import net.minecraft.item.map.MapBannerMarker;
+import net.minecraft.util.math.BlockPos;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import union.xenfork.nucleoplasm.json.edit.face.net.minecraft.item.map.GetMapBannerMarker;
 
 @Mixin(MapBannerMarker.class)
 public class MixinMapBannerMarker implements GetMapBannerMarker {
 
+    @Shadow
+    @Final
+    private BlockPos pos;
+
+    @Override
+    public BlockPos getPos() {
+        return pos;
+    }
+
+    @Override
+    public void setPos(BlockPos pos) {
+
+    }
 }
