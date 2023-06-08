5c8314b0521966a5767f0c014accc8dbe21a5c03
baka4n <qq474899581@gmail.com>
2023-06-09 03:26:09 +0800

    Update LoginCommand.java

--- nucleoplasm/normandy-login/src/main/java/union/xenfork/nucleoplasm/normandy/login/command/LoginCommand.java
+++ nucleoplasm/normandy-login/src/main/java/union/xenfork/nucleoplasm/normandy/login/command/LoginCommand.java
@@ -7,6 +7,7 @@ import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import com.mojang.brigadier.exceptions.SimpleCommandExceptionType;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.Text;
 import union.xenfork.nucleoplasm.api.sql.NucleoplasmEntity;
 import union.xenfork.nucleoplasm.normandy.login.NucleoplasmServer;
@@ -14,7 +15,7 @@ import union.xenfork.nucleoplasm.normandy.login.NucleoplasmServer;
 public class LoginCommand implements Command<ServerCommandSource> {
     @Override
     public int run(CommandContext<ServerCommandSource> context) throws CommandSyntaxException {
-        PlayerEntity player = context.getSource().getPlayer();
+        var player = context.getSource().getPlayer();
         String password = context.getArgument("password", String.class);
         if (player != null) {
             NucleoplasmEntity entity = NucleoplasmServer.nnl.findEntity(player);
