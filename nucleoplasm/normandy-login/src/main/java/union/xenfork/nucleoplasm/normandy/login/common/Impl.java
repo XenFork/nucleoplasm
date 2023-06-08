fb4fb8c0a4fd7083bd53e87834abf8c9d1dab0e9
baka4n <qq474899581@gmail.com>
2023-06-08 20:59:32 +0800

    Delete Impl.java

--- nucleoplasm/normandy-login/src/main/java/union/xenfork/nucleoplasm/normandy/login/common/Impl.java
+++
@@ -1,8 +0,0 @@
-package union.xenfork.nucleoplasm.normandy.login.common;
-
-import com.mojang.brigadier.CommandDispatcher;
-import net.minecraft.server.command.ServerCommandSource;
-
-public interface Impl {
-    void register(CommandDispatcher<ServerCommandSource> dispatcher);
-}
