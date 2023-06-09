07e04fe53e67881063c4cc41f4858189052343ea
baka4n <qq474899581@gmail.com>
2023-06-09 16:38:09 +0800

    更新

---
+++ nucleoplasm/api/src/main/java/union/xenfork/nucleoplasm/api/NucleoplasmServerTest.java
@@ -0,0 +1,9 @@
+package union.xenfork.nucleoplasm.api;
+
+import net.fabricmc.api.DedicatedServerModInitializer;
+
+public class NucleoplasmServerTest implements DedicatedServerModInitializer {
+    @Override
+    public void onInitializeServer() {
+    }
+}
