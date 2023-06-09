bd1cc0bf6df43461e993960c41ee79b9aea55bf9
baka4n <qq474899581@gmail.com>
2023-06-09 14:35:24 +0800

    update

--- nucleoplasm/diggers/src/main/java/union/xenfork/nucleoplasm/diggers/NucleoplasmServer.java
+++ nucleoplasm/diggers/src/main/java/union/xenfork/nucleoplasm/diggers/NucleoplasmServer.java
@@ -3,9 +3,7 @@ package union.xenfork.nucleoplasm.diggers;
 import net.fabricmc.api.DedicatedServerModInitializer;
 
 public class NucleoplasmServer implements DedicatedServerModInitializer {
-    
-    @Override
-    public void onInitializeServer() {
 
-    }
+    @Override
+    public void onInitializeServer() {}
 }
