07cc2fe5ccb7e4ab1d5fabb401b8ffc5b6c29a10
baka4n <qq474899581@gmail.com>
2023-06-09 12:55:50 +0800

    添加伐伐伐木工模组

--- nucleoplasm/fell-fell-lumberjack/src/main/java/union/xenfork/nucleoplasm/fell/fell/lumberjack/NucleoplasmServer.java
+++ nucleoplasm/fell-fell-lumberjack/src/main/java/union/xenfork/nucleoplasm/fell/fell/lumberjack/NucleoplasmServer.java
@@ -1,9 +1,8 @@
-package union.xenfork.nucleoplasm.diggers;
+package union.xenfork.nucleoplasm.fell.fell.lumberjack;
 
 import net.fabricmc.api.DedicatedServerModInitializer;
 
 public class NucleoplasmServer implements DedicatedServerModInitializer {
-    
     @Override
     public void onInitializeServer() {
 
