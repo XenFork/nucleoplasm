997e61c88b5a07b8738f6744fcf76c3e4f69f91b
baka4n <qq474899581@gmail.com>
2023-06-09 12:43:24 +0800

    添加连锁挖掘

---
+++ nucleoplasm/diggers/src/main/java/union/xenfork/nucleoplasm/diggers/NucleoplasmServer.java
@@ -0,0 +1,10 @@
+package union.xenfork.nucleoplasm.diggers;
+
+import net.fabricmc.api.ModInitializer;
+
+public class NucleoplasmServer implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
