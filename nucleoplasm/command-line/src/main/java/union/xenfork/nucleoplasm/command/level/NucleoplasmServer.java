6306a6d3117bb31a6fafcf3c8902728c5ffe85f2
baka4n <qq474899581@gmail.com>
2023-06-11 02:20:57 +0800

    Update NucleoplasmServer.java

--- nucleoplasm/command-line/src/main/java/union/xenfork/nucleoplasm/command/level/NucleoplasmServer.java
+++ nucleoplasm/command-line/src/main/java/union/xenfork/nucleoplasm/command/level/NucleoplasmServer.java
@@ -4,7 +4,6 @@ import net.fabricmc.api.DedicatedServerModInitializer;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
 import net.minecraft.server.network.ServerPlayerEntity;
-import union.xenfork.nucleoplasm.api.config.Config;
 import union.xenfork.nucleoplasm.api.event.ServerPlayerEvents;
 
 public final class NucleoplasmServer implements DedicatedServerModInitializer {
