013ed3e6b378adf125beb0033ef78024ee019d0a
baka4n <qq474899581@gmail.com>
2023-06-26 00:27:13 +0800

    nbt序列化

--- nucleoplasm/api/src/main/java/union/xenfork/nucleoplasm/api/mixin/serializable/nbt/NbtCompoundMixin.java
+++ nucleoplasm/api/src/main/java/union/xenfork/nucleoplasm/api/mixin/serializable/nbt/NbtCompoundMixin.java
@@ -1,16 +1,14 @@
 package union.xenfork.nucleoplasm.api.mixin.serializable.nbt;
 
-import net.minecraft.nbt.NbtString;
+import net.minecraft.nbt.NbtCompound;
 import org.spongepowered.asm.mixin.Debug;
 import org.spongepowered.asm.mixin.Mixin;
 
 import java.io.Serial;
 import java.io.Serializable;
-
 @Debug(export = true)
-@Mixin(NbtString.class)
-public class NbtStringMixin implements Serializable {
-
+@Mixin(NbtCompound.class)
+public class NbtCompoundMixin implements Serializable {
     @Serial
-    private static final long serialVersionUID = -4692153822315647693L;//序列化Vec3d的测试
+    private static final long serialVersionUID = -6652179279718398446L;
 }
