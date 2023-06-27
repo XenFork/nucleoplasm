33dfe8a2c7e3430792aef1a1f9f17e3d69703525
baka4n <qq474899581@gmail.com>
2023-06-27 19:12:13 +0800

    注册事件

--- nucleoplasm/registry/src/main/java/union/xenfork/nucleoplasm/registry/Nucleoplasm.java
+++ nucleoplasm/registry/src/main/java/union/xenfork/nucleoplasm/registry/Nucleoplasm.java
@@ -1,16 +1,11 @@
 package union.xenfork.nucleoplasm.registry;
 
 import net.fabricmc.api.ModInitializer;
-import net.minecraft.item.Item;
-import union.xenfork.nucleoplasm.registry.event.RegistryEvent;
 import union.xenfork.nucleoplasm.registry.registry.ModItems;
 
 public class Nucleoplasm implements ModInitializer {
     @Override
     public void onInitialize() {
-        RegistryEvent.REGISTRY_ITEM_EVENT.register(modItem -> {
-            modItem.add("test", Item::new);
-        });
         ModItems.registry();
     }
 }
