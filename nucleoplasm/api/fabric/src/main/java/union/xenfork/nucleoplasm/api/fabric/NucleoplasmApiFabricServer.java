d5be784148a6d97833792e9c6a11e3c8ee035ab0
baka4n <qq474899581@gmail.com>
2023-06-29 22:10:37 +0800

    Update NucleoplasmApiFabricServer.java

--- nucleoplasm/api/fabric/src/main/java/union/xenfork/nucleoplasm/api/fabric/NucleoplasmApiFabricServer.java
+++ nucleoplasm/api/fabric/src/main/java/union/xenfork/nucleoplasm/api/fabric/NucleoplasmApiFabricServer.java
@@ -1,11 +1,9 @@
 package union.xenfork.nucleoplasm.api.fabric;
 
 import net.fabricmc.api.DedicatedServerModInitializer;
-import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 
 public class NucleoplasmApiFabricServer implements DedicatedServerModInitializer {
     @Override
     public void onInitializeServer() {
-        ServerLifecycleEvents.SERVER_STARTING.register(union.xenfork.nucleoplasm.event.lifecycle.ServerLifecycleEvents.SERVER_START_EVENT.invoker()::onServerStarted);
     }
 }
