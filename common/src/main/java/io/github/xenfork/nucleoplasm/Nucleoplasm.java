45c8021a438346de7470b895256de625b2180ecb
baka4n <qq474899581@gmail.com>
2023-07-30 21:42:54 +0800

    添加groups

--- common/src/main/java/io/github/xenfork/nucleoplasm/Nucleoplasm.java
+++ common/src/main/java/io/github/xenfork/nucleoplasm/Nucleoplasm.java
@@ -1,7 +1,22 @@
 package io.github.xenfork.nucleoplasm;
 
+import dev.architectury.registry.CreativeTabRegistry;
+import io.github.xenfork.nucleoplasm.core.Utils;
+import io.github.xenfork.nucleoplasm.core.item.Inorganic$Item;
+import io.github.xenfork.nucleoplasm.core.item.Organic$Matter$Item;
 import io.github.xenfork.nucleoplasm.registry.Groups;
 import io.github.xenfork.nucleoplasm.registry.Items;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.util.Identifier;
+
+import java.util.Arrays;
+
+import static io.github.xenfork.nucleoplasm.core.Utils.nums;
 
 public class Nucleoplasm
 {
@@ -10,7 +25,31 @@ public class Nucleoplasm
 	public static void init() {
 		Items.init();
 		Groups.init();
+		CreativeTabRegistry.modify(CreativeTabRegistry.defer(Groups.elements.getRegistryKey()), (flags, output, canUseGameMasterBlocks) -> {
+			Item item = Registries.ITEM.get(new Identifier(MOD_ID, "inorganic"));
+			output.add(elemental(item,"H", nums[2]));
+			output.add(elemental(item,"He"));
+			output.add(elemental(item,"Li"));
+			output.add(elemental(item,"Be"));
+			output.add(elemental(item,"B", nums[2]));
+			output.add(elemental(item,"B", nums[1], nums[0]));
+			output.add(elemental(item,"B", nums[1], nums[2]));
+			output.add(elemental(item,"B", nums[4], nums[0]));
+			output.add(elemental(item, "C", nums[1]));
+			output.add(elemental(item, "C", nums[6], nums[0]));
+			output.add(elemental(item, "C", nums[3], nums[6]));
+			output.add(elemental(item, "C", nums[7], nums[0]));
+			output.add(elemental(item, "C", nums[8], nums[4]));
+			output.add(elemental(item, "C", nums[2], nums[4], nums[0]));
+			output.add(elemental(item, "C", nums[5], nums[4], nums[0]));
+		});
+	}
 
-
+	public static ItemStack elemental(Item item, String... cfs) {
+		ItemStack itemStack = new ItemStack(item);
+		NbtCompound nbt = new NbtCompound();
+		nbt.putString("cf", Arrays.toString(cfs));
+		itemStack.setNbt(nbt);
+		return itemStack;
 	}
 }
--- common/src/main/java/io/github/xenfork/nucleoplasm/core/item/Inorganic$Item.java
+++ common/src/main/java/io/github/xenfork/nucleoplasm/core/item/Inorganic$Item.java
@@ -8,6 +8,7 @@ import net.minecraft.entity.Entity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
+import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.NotNull;
@@ -37,77 +38,14 @@ public class Inorganic$Item extends Item {
         if (nbt == null) {
             return;
         }
-        Serializer serializer = new Serializer(nbt);
-        StringBuilder sb = new StringBuilder();
-        for (int i = 1; i <= serializer.count; i++) {
-            String an = serializer.nucleoplasm.getString("an_" + i);
-            int count = serializer.nucleoplasm.getInt("count_" + i);
-            if (!Utils.others.contains(an)) {
-                sb.append(an).append(Utils.nums[count]);
-            } else {
-                sb.append("(").append(an).append(")").append(Utils.nums[count]);
-            }
-        }
-        tooltip.add(Text.translatable("nucleoplasm.chemical.formula").append(sb.toString()));
-
-    }
-
-    @Override
-    public String getTranslationKey(ItemStack stack) {
-        NbtCompound nbt = stack.getNbt();
-        if (nbt == null || !nbt.contains("nucleoplasm")) {
-            return super.getTranslationKey(stack);
-        }
-        Serializer serializer = new Serializer(nbt);
-        StringBuilder sb = new StringBuilder();
-        for (int i = 1; i <= serializer.count; i++) {
-            String an = serializer.nucleoplasm.getString("an_" + i);
-            int count = serializer.nucleoplasm.getInt("count_" + i);
-            if (!Utils.others.contains(an)) {
-                sb.append(an).append(Utils.nums[count]);
-            } else {
-                sb.append("(").append(an).append(")").append(Utils.nums[count]);
-            }
-        }
-        return super.getTranslationKey(stack) + "." + sb;
-    }
-
-    public static class Serializer implements SerializerImpl {
-        NbtCompound nucleoplasm;
-        int count;//决定了有多少物质组成
-        /*
-        300为OH-离子
-         */
-
-        public Serializer(NbtCompound nbt) {
-            nucleoplasm = nbt.contains("nucleoplasm") ? nbt.getCompound("nucleoplasm") : new NbtCompound();
-            count = nucleoplasm.contains("count") ? nbt.getInt("count") : 0;
+        if (nbt.contains("cf")) {
+            String string = nbt.getString("cf");
+            MutableText translatable = Text.translatable("nucleoplasm.chemical.formula");
+            tooltip.add(translatable.append(string));
         }
 
-        public void add(String an, int count) {// 原子序数,和个数
-            this.count++;
-            nucleoplasm.putString("an_" + this.count, an);
-            nucleoplasm.putInt("count_" + this.count, count);
-        }
 
-        public void clear() {
-            for (int i = count; i > 0; i--) {
-                nucleoplasm.remove("an_" +count);
-                nucleoplasm.remove("count_" +count);
-            }
-            count = 0;
-        }
 
-        @Override
-        public void deserializer(@NotNull NbtCompound nbt) {
-            nucleoplasm.putInt("count", count);
-            nbt.put("nucleoplasm", nucleoplasm);
-        }
-
-        @Override
-        public NbtCompound getNbt() {
-            return nucleoplasm.copy();
-        }
     }
 
 }
