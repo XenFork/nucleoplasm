da2e969ddcbfdc4860488324316abb12ae3bbda7
baka4n <qq474899581@gmail.com>
2023-08-12 16:44:00 +0800

    change priority

--- common/src/main/java/io/github/xenfork/nucleoplasm/mixin/forge/MinecraftItemsMixin.java
+++ common/src/main/java/io/github/xenfork/nucleoplasm/mixin/forge/MinecraftItemsMixin.java
@@ -19,7 +19,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(value = Items.class, priority = 0)
+@Mixin(value = Items.class, priority = 114514)
 public class MinecraftItemsMixin {
     @Mutable
     @Shadow @Final public static Item IRON_ORE;
