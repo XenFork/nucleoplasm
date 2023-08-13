70c5eb45522e39eeadc8ee7d9f3e13f00304f1a1
baka4n <qq474899581@gmail.com>
2023-08-13 15:29:18 +0800

    添加tooltip

--- common/src/main/java/io/github/xenfork/nucleoplasm/mixin/forge/MinecraftItemMixin.java
+++ common/src/main/java/io/github/xenfork/nucleoplasm/mixin/forge/MinecraftItemMixin.java
@@ -1,16 +1,20 @@
 package io.github.xenfork.nucleoplasm.mixin.forge;
 
+import net.minecraft.client.item.TooltipContext;
 import net.minecraft.entity.Entity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.registry.Registries;
+import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.List;
 
 @Mixin(Item.class)
 public class MinecraftItemMixin {
@@ -23,12 +27,24 @@ public class MinecraftItemMixin {
 
         if (id.getNamespace().equals("minecraft")) {
             switch (id.getPath()) {
+                // 栎树(橡树) 植物界 种子植物门 木兰纲 壳斗目 栎属 
+                //
                 /*ore*/case "gold_ore", "deepslate_gold_ore", "iron_ore", "deepslate_iron_ore", "coal_ore", "deepslate_coal_ore", "nether_gold_ore", "lapis_ore", "deepslate_lapis_ore" , "diamond_ore", "deepslate_diamond_ore" -> /*真正写到的时候再分层*/resetNucleoplasmName(stack, "ores");
                 /*log*/case "oak_log", "spruce_log","birch_log", "jungle_log", "acacia_log", "cherry_log", "dark_oak_log", "mangrove_log" -> resetNucleoplasmName(stack, "logs");
                 /*Stripped log*/case "stripped_spruce_log", "stripped_birch_log", "stripped_jungle_log", "stripped_acacia_log", "stripped_cherry_log", "stripped_dark_oak_log", "stripped_oak_log", "stripped_mangrove_log" -> resetNucleoplasmName(stack, "stripped logs");
                 /*wood*/case "oak_wood", "spruce_wood", "birch_wood", "jungle_wood", "acacia_wood", "cherry_wood", "dark_oak_wood", "mangrove_wood" -> resetNucleoplasmName(stack, "wood");
                 /*stripped wood*/case "stripped_oak_wood", "stripped_spruce_wood", "stripped_birch_wood", "stripped_jungle_wood", "stripped_acacia_wood","stripped_cherry_wood", "stripped_dark_oak_wood", "stripped_mangrove_wood" -> resetNucleoplasmName(stack, "stripped wood");
                 /*leaves*/case "oak_leaves", "spruce_leaves", "birch_leaves", "jungle_leaves", "acacia_leaves", "cherry_leaves", "dark_oak_leaves", "mangrove_leaves", "azalea_leaves", "flowering_azalea_leaves" -> resetNucleoplasmName(stack, "leaves");
+            }
+        }
+    }
+
+    @Inject(method = "appendTooltip", at = @At("RETURN"))
+    private void appendTooltip(ItemStack stack, World world, List<Text> tooltip, TooltipContext context, CallbackInfo ci) {
+        if (stack.getNbt() != null) {
+            NbtCompound nbt = stack.getNbt();
+            if (nbt.contains(nucleoplasmName)) {
+                tooltip.add(Text.translatable("nucleoplasm.designation").append(Text.translatable("nucleoplasm." + nbt.getString(nucleoplasmName))));
             }
         }
     }
