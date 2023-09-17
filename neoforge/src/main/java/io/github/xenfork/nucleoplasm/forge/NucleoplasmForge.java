ee1886394618585cd87c7035f95d266ced18dc34
baka4n <qq474899581@gmail.com>
2023-09-18 01:23:10 +0800

    add the neoforge

---
+++ neoforge/src/main/java/io/github/xenfork/nucleoplasm/forge/NucleoplasmForge.java
@@ -0,0 +1,15 @@
+package io.github.xenfork.nucleoplasm.forge;
+
+import dev.architectury.platform.forge.EventBuses;
+import io.github.xenfork.nucleoplasm.Nucleoplasm;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod(Nucleoplasm.MOD_ID)
+public class NucleoplasmForge {
+    public NucleoplasmForge() {
+		// Submit our event bus to let architectury register our content on the right time
+        EventBuses.registerModEventBus(Nucleoplasm.MOD_ID, FMLJavaModLoadingContext.get().getModEventBus());
+        Nucleoplasm.init();
+    }
+}
